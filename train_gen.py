8eee1af2b1ed2c56128240acb08827e7eef519cd
Shapely <melon73ru@mail.ru>
2018-06-27 17:39:12 +0400

    gaaan!

--- train_gen.py
+++ train_gen.py
@@ -1,48 +1,71 @@
+# -*- coding: utf-8 -*-
+
 import os
 import cv2
 import random
+from os import listdir 
+from os.path import isfile, join 
 
 class Gen:
-	def __init__(self, data_info, Xfolder, Yfolder, train_pers, input_size):
+	def __init__(self, data_info, Xfolder, Yfolder, Testfloder,train_pers, input_size):
 		self.train_pers = train_pers
 		self.input_size = input_size
 		self.x_folder = Xfolder
 		self.y_folder = Yfolder
+		self.test_floder = Testfloder
 
-		self.x_files = []
-		self.y_files = []
+		self.files = []
 		data = open(data_info, 'r')
 		for line in data.readlines():
-			files = line.split()
-			if(len(files) == 0):
+			file = line.split()
+			if(len(file) == 0):
 				continue
-			self.y_files.append(files[0])
-			x_files = []
-			for i in range(1, len(files)):
-				x_files.append(files[i])
-			self.x_files.append(x_files)
+			for i in range(1, len(file)):
+				self.files.append([file[0], file[i]])
+		random.seed(1337)
+		random.shuffle(self.files)			
 
-	def get_batch(self, batch_size, is_training = True):
+	def get_epoch(self, batch_size, is_training = True):
 		x_batch = []
 		y_batch = []
-		for i in range(0, batch_size):
-			if(is_training):
-				number = random.randint(0, int(len(self.x_files) * self.train_pers))
-			else:
-				number = random.randint(int(len(self.x_files) * self.train_pers), len(self.x_files) - 1)
-			number2 =  random.randint(0, len(self.x_files[number]) - 1)
 
-			X = cv2.imread(self.x_folder + self.x_files[number][number2])
-			X = cv2.resize(X, (self.input_size,self.input_size))
+		if is_training:
+			for j in range(	0, int(len(self.files) * 0.8) // batch_size ):	
+				x_batch.append([])
+				y_batch.append([])
+				
+				for i in range(0, batch_size):
+					X = cv2.imread(self.x_folder + self.files[j * batch_size + i][1])
+					Y = cv2.imread(self.y_folder + self.files[j * batch_size + i][0])
+					x_batch[j].append(X)
+					y_batch[j].append(Y)
+		else:
+			for j in range(	int(len(self.files) * 0.8) // batch_size , len(self.files) // batch_size ):	
+				x_batch.append([])
+				y_batch.append([])
 
-			Y = cv2.imread(self.y_folder + self.y_files[number])
-			Y = cv2.resize(Y, (self.input_size,self.input_size))
-
-			x_batch.append(X)
-			y_batch.append(Y)
+				for i in range(0, batch_size):
+					X = cv2.imread(self.x_folder + self.files[j * batch_size + i][1])
+					Y = cv2.imread(self.y_folder + self.files[j * batch_size + i][0])
+					
+					x_batch[len(x_batch) - 1].append(X)
+					y_batch[len(y_batch) - 1].append(Y)
+		
 		return x_batch, y_batch	
+	def get_test(self):
 
+		files = []
+		for f in listdir(self.test_floder):
+			f2 = join(self.test_floder, f)
+			if isfile(f2):
+				files.append(f2)
 
+		print(files)
+		images = [None] * len(files)
+		for i in range(len(files)):
+			images[i] = cv2.resize(cv2.imread(files[i]), (512, 512))
+
+		return images
 #gen = Gen('kak_ygodno.txt', 'images/X/', 'images/Y/', 80, 256)
 
 #X, Y = gen.get_batch(5)
