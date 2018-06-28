d6c86438a103edb55fa9ffe245eabd29c12cae98
Shapely <melon73ru@mail.ru>
2018-06-28 11:39:54 +0400

    commit

--- train_gen.py
+++ train_gen.py
@@ -37,7 +37,7 @@ class Gen:
 				x_batch.append(X)
 				y_batch.append(Y)
 			self.j +=1
-
+            print(self.j , x_batch, y_batch)
 			if self.j > len(self.files) // batch_size:
 				self.j=0
 		else:
