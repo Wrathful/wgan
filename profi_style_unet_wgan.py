11baaf68d284bdfdcc85e2ee079fde8e70053d25
Shapely <melon73ru@mail.ru>
2018-06-29 13:39:05 +0400

    fdsa

--- profi_style_unet_wgan.py
+++ profi_style_unet_wgan.py
@@ -210,7 +210,7 @@ def make_discriminator():
         model.add(Convolution2D(64, (5, 5), padding='same', input_shape=(3, 512, 512)))
     else:
         model.add(Convolution2D(64, (5, 5), padding='same', input_shape=(512, 512, 3)))
-    model.add(Convolution2D(64, (5, 5), kernel_initializer='he_normal', strides=[2, 2]))
+    model.add(Convolution2D(64, (5, 5), kernel_initializer='he_normal',padding='same', strides=[2, 2]))
     model.add(MaxPooling2D(pool_size=2))
     model.add(LeakyReLU())
     model.add(Convolution2D(128, (5, 5), kernel_initializer='he_normal', strides=[2, 2]))
